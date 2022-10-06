2d161920f035d8272f37025825b53ad8ebd64398
ankhudozhilova <anna250303@outlook.com>
2022-10-06 10:39:20 +0300

    lab_1

---
+++ Lab_it_p/src/Palindrome.java
@@ -0,0 +1,21 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String s = args[i];
+            if(isPalindrome(args[i]))
+                System.out.println("palindrome");
+            else
+                System.out.println("not palindrome");
+        }
+    }
+    public static String reverseString(String s){
+        String a = "";
+        for(int i = s.length() - 1; i >= 0; i--){
+            a += (s.charAt(i));
+        }
+        return a;
+    }
+    public static boolean isPalindrome(String s){
+        return s.equals(reverseString(s));
+    }
+}
